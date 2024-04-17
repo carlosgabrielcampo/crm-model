4550a258f957dbf94e81b0aef553ba6243cdc108
Carlos Campo <carloscampo@Carloss-MacBook-Pro.local>
2024-04-17 19:45:31 -0300

    created first routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<></>}/>
+        <Route path="/home" element={<></>}/>
+        <Route path="/financeiro" element={<></>}/>
+        <Route path="/cadastro" element={<></>}/>
+        <Route path="/cadastro/clientes" element={<></>}/>
+        <Route path="/cadastro/usuarios" element={<></>}/>
+        <Route path="/funil" element={<></>}/>
+        <Route path="/configuracoes" element={<></>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
