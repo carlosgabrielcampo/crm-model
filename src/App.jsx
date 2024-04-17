8ea564ee5f26da64e7b622de7400a7a42bb75904
Carlos Campo <carlosgabrielcampo@gmail.com>
2024-04-17 20:25:31 -0300

    Auth Context updatedand General Context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./config/context/auth/index.js";
+import CRMProvider from "./config/context/provider/index.jsx";
 import Router from "./routes";
 
 function App() {
   return (
-    <Router />
+      <AuthProvider>
+        <BrowserRouter>
+          <CRMProvider>
+            <Router />
+          </CRMProvider>
+        </BrowserRouter>
+      </AuthProvider>
   );
 }
 
