187930731b6a284a1edabc030c9f2f818ff429e1
Carlos Campo <carloscampo@Carloss-MacBook-Pro.local>
2024-04-17 19:56:26 -0300

    model routes created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import Router from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<></>}/>
-        <Route path="/home" element={<></>}/>
-        <Route path="/financeiro" element={<></>}/>
-        <Route path="/cadastro" element={<></>}/>
-        <Route path="/cadastro/clientes" element={<></>}/>
-        <Route path="/cadastro/usuarios" element={<></>}/>
-        <Route path="/funil" element={<></>}/>
-        <Route path="/configuracoes" element={<></>}/>
-      </Routes>
-    </BrowserRouter>
+    <Router />
   );
 }
 
